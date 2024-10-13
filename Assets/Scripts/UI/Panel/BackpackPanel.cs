f8679f6459682df80897ea04edafc85c0c80c1ce
bbbasya <42473270+asyaB404@users.noreply.github.com>
2024-10-13 21:06:00 +0800

    no message

---
+++ Assets/Scripts/UI/Panel/BackpackPanel.cs
@@ -0,0 +1,10 @@
+namespace UI.Panel
+{
+    public class BackpackPanel : BasePanel<BackpackPanel>
+    {
+        public override void Init()
+        {
+            base.Init();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Panel/GamePanel.cs
@@ -0,0 +1,17 @@
+using UnityEngine.UI;
+
+namespace UI.Panel
+{
+    public class GamePanel : BasePanel<GamePanel>
+    {
+        public override void Init()
+        {
+            base.Init();
+            GetControl<Button>("BackpackBtn").onClick.AddListener(() => { });
+        }
+
+        public override void CallBack(bool flag)
+        {
+        }
+    }
+}

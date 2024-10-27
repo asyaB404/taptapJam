8babae776a4d0d991a6f3e794ac558cad431c973
bbbasya <42473270+asyaB404@users.noreply.github.com>
2024-10-27 13:25:20 +0800

    no message

--- Assets/Test/StartButton.cs
+++ Assets/Test/StartButton.cs
@@ -4,11 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField] private Scene scene;
 
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("开始游戏");
-        SceneManager.LoadScene(nameof(scene));
+        SceneManager.LoadScene("Main");
     }
 }
